b5b04b28a71e67789006e76fac8f3fae9ab64c2d
Aastha Kesarwani <aastha.baloda@gmail.com>
2016-08-09 10:18:16 +0530

    Day 1 training

---
+++ src/search_bar.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';  //Will only import component from React
+
+class SearchBar extends Component {
+	render() {
+		return (
+			<div>
+				<input />
+			</div>
+		);
+	}
+}
+
+export default SearchBar;
